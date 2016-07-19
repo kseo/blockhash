c52edda862e806ce8592b7f4b13999bc551ba673
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-07-19 10:13:11 +0900

    Add option parser to the blockhash app

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,37 @@
 module Main where
 
 import Data.Blockhash
+import Options.Applicative
+
+data Option = Option
+  { quickFlag :: Bool
+  , bits :: Int
+  , debugFlag :: Bool
+  , filenames :: [FilePath]
+  } deriving (Show)
+
+opts = info (helper <*> cmdOpt)
+  ( fullDesc
+  <> progDesc "blockhash"
+  )
+  where
+    cmdOpt = Option
+       <$> switch
+        ( short 'q'
+        <> long "quick"
+        <> help "Use quick hashing method")
+       <*> option (auto :: ReadM Int)
+        ( long "bits"
+        <> short 'b'
+        <> value 16
+        <> help "Create hash of size N^2 bits." )
+       <*> switch
+        ( long "debug"
+        <> help "Print hashes as 2D maps (for debugging)" )
+       <*> some (argument str (metavar "filenames"))
 
 main :: IO ()
-main = putStrLn "blockhash"
+main = do
+  option <- execParser opts
+  print option
+
