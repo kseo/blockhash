0bd80afa39923b3e782c453578db353afa2e10f5
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-07-19 11:01:37 +0900

    Implement stackhash app

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,15 @@
 module Main where
 
+import qualified Codec.Picture as P
+import Control.Monad (forM_)
 import Data.Blockhash
+import qualified Data.Vector.Generic as VG
+import qualified Data.Vector.Unboxed as V
 import Options.Applicative
 
 data Option = Option
   { quickFlag :: Bool
   , bits :: Int
-  , debugFlag :: Bool
   , filenames :: [FilePath]
   } deriving (Show)
 
@@ -25,13 +28,26 @@ opts = info (helper <*> cmdOpt)
         <> short 'b'
         <> value 16
         <> help "Create hash of size N^2 bits." )
-       <*> switch
-        ( long "debug"
-        <> help "Print hashes as 2D maps (for debugging)" )
        <*> some (argument str (metavar "filenames"))
+
+processImage :: FilePath -> Int -> Bool -> IO ()
+processImage filename bits quick = do
+  res <- P.readImage filename
+  case res of
+    Left err -> putStrLn ("Fail to read: " ++ filename)
+    Right dynamicImage -> do
+      let rgbaImage = P.convertRGBA8 dynamicImage
+          pixels = VG.convert (P.imageData rgbaImage)
+          image = Image { imagePixels = pixels
+                        , imageWidth = P.imageWidth rgbaImage
+                        , imageHeight = P.imageHeight rgbaImage }
+          method = if quick then Quick else Precise
+          hash = blockhash image bits method
+      putStrLn (show hash ++ "  " ++ filename)
 
 main :: IO ()
 main = do
   option <- execParser opts
-  print option
+  forM_ (filenames option) (\filename ->
+    processImage filename (bits option) (quickFlag option))
 
