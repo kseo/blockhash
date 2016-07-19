76965c247745a46975eb4d5449c3e3ce1f58ba38
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-07-19 11:02:51 +0900

    Import test data and script from https://github.com/commonsmachinery/blockhash/

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+d=`dirname "$0"`
+
+cd "$d/testdata"
+echo
+blockhash -b 16 *.jpg *.png | sort | diff -su exact-hashes.txt -
+echo
+blockhash -q -b 16 *.jpg *.png | sort | diff -su quick-hashes.txt -
+echo
+echo OK
+echo
